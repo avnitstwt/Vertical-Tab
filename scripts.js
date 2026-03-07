badcc9052a6da30fdd5170d5ca35dfcfc24919b5
Abhya <abhisheksingh.cs19@gmail.com>
2026-03-07 18:35:34 +0530

    Final Push

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const contentDivs = document.querySelectorAll('.right > div');
+const radios = document.querySelectorAll('input[type="radio"]');
+
+console.log(contentDivs);
+
+function updateDisplay(id) {
+    contentDivs.forEach(div => {
+        const targetClass = id.charAt(0).toUpperCase() + id.slice(1);
+        
+        if (div.classList.contains(targetClass)) {
+            div.style.display = 'block';
+        } else {
+            div.style.display = 'none';
+        }
+    });
+}
+
+radios.forEach(radio => {
+    radio.addEventListener('change', (e) => {
+        updateDisplay(e.target.id);
+    });
+});
+
+updateDisplay('home');
